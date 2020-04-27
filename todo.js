572899a7b6ffa66ccdf95299459a5ce02b0b1de9
saintluxpgw <saintluxpgw@gmail.com>
2020-04-27 23:36:45 +0900

    todo list created

---
+++ todo.js
@@ -0,0 +1,57 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDOlist = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("buttom");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "🆇 ";
+    span.innerText = text
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
+    toDOlist.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSumnit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function (toDo) {
+            paintToDo(toDo.text);
+        });
+    } else {
+
+    }
+}
+
+
+function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSumnit)
+}
+
+init();
